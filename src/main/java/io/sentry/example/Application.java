b1fb7db32fab013222921e0ae3eb49f1366f7ea6
Brett Hoerner <brett@bretthoerner.com>
2018-02-28 13:18:41 -0800

    Minor cleanup and updates.

--- src/main/java/io/sentry/example/Application.java
+++ src/main/java/io/sentry/example/Application.java
@@ -4,12 +4,9 @@ import org.apache.logging.log4j.ThreadContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,11 +22,28 @@ public class Application {
     String home() {
         ThreadContext.put("customKey1", "customValue1");
         ThreadContext.put("customKey2", "customValue2");
-        logger.warn("Warn message"); //warning message that will be sent to Sentry
 
-        int x = 1 / 0; //uncaught exception that will be sent to Sentry
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warn message"); // warning message that will be sent to Sentry
+
+        handledError();
+        unhandledError();
 
         return "Hello World";
+    }
+
+    private void handledError() {
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            // caught exception that will be sent to Sentry
+            logger.error("Caught exception!", e);
+        }
+    }
+
+    private void unhandledError() {
+        throw new RuntimeException("Unhandled exception!");
     }
 
     public static void main(String[] args) {
